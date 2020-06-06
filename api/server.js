4de39780d1168983359ec59b8488083b89f20d9c
tsbarrett89 <tsbarrett89@gmail.com>
2020-06-06 12:10:43 -0600

    added helmet to server

--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const helmet = require('helmet')
 
 const patternsRouter = require('../patterns/patternsRouter')
 const ordersRouter = require('../orders/ordersRouter')
 const imagesRouter = require('../images/imagesRouter')
 
 const server = express()
+
+server.use(helmet())
+server.use(express.json())
 
 server.get('/', (req, res) => {
     res.send('Elisabeth Artistry API')
