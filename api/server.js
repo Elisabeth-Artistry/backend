95ee8771c1676c68dddcc55782d91d33558f73dd
tsbarrett89 <tsbarrett89@gmail.com>
2020-06-06 12:06:11 -0600

    patterns, orders, images routers added and used

--- api/server.js
+++ api/server.js
@@ -1,9 +1,17 @@
 const express = require('express')
 
+const patternsRouter = require('../patterns/patternsRouter')
+const ordersRouter = require('../orders/ordersRouter')
+const imagesRouter = require('../images/imagesRouter')
+
 const server = express()
 
 server.get('/', (req, res) => {
     res.send('Elisabeth Artistry API')
 })
+
+server.use('/api/patterns', patternsRouter)
+server.use('/api/orders', ordersRouter)
+server.use('/api/images', imagesRouter)
 
 module.exports = server
