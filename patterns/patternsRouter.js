b555e679b82a6efd20cfe347aea0208442251cea
tsbarrett89 <tsbarrett89@gmail.com>
2020-06-06 13:49:39 -0600

    post route created

--- patterns/patternsRouter.js
+++ patterns/patternsRouter.js
@@ -1,4 +1,27 @@
 const express = require('express')
+const patterns = require('./patternsModel')
+
 const router = express.Router()
+
+router.post('/create', (req, res) => {
+    const details = req.body
+
+    if(
+        details.name &&
+        details.image_url &&
+        details.description &&
+        details.price
+    ){
+        patterns.add(details)
+            .then(patternId => {
+                res.status(201).json({ id: patternId})
+            })
+            .catch(error => {
+                res.status(409).json({errorMessage: `${details.name} already exists`})
+            })
+    } else {
+        res.status(400).json({ errorMessage: "name, image_url, description, and price are all required"})
+    }
+})
 
 module.exports = router
