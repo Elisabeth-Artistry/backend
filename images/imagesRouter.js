f234d8c5012197d5537083b0c545f8399a8d9f03
tsbarrett89 <tsbarrett89@gmail.com>
2020-06-06 21:07:17 -0600

    router.post created

--- images/imagesRouter.js
+++ images/imagesRouter.js
@@ -1,4 +1,36 @@
 const express = require('express')
+const images = require('./imagesModel')
+
 const router = express.Router()
+
+router.post('/create', (req, res) => {
+    const details = req.body
+
+    if(
+        details.name &&
+        details.image_url &&
+        details.category
+    ){
+        images.findByName(details.name)
+            .then(imageArr => {
+                if(imageArr.length > 0){
+                    res.status(409).json({ message: `${imageArr[0].name} already exists`})
+                } else {
+                    images.add(details)
+                        .then(imageId => {
+                            res.status(201).json({ id: imageId, ...details })
+                        })
+                        .catch(error => {
+                            res.status(500).json({ errorMessage: 'unable to add image'})
+                        })
+                }
+            })
+            .catch(error => {
+                res.status(500).json({ errorMessage: "unable to add image"})
+            })
+    } else {
+        res.status(400).json({ errorMessage: "name, image_url, and category are all required"})
+    }
+})
 
 module.exports = router
