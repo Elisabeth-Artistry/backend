42647580487530c33ec915e76d99288252c91189
tsbarrett89 <tsbarrett89@gmail.com>
2020-06-08 14:55:45 -0600

    cors issue

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ const server = express()
 
 server.use(helmet())
 server.use(express.json())
-server.use(cors)
+server.use(cors())
 
 server.get('/', (req, res) => {
     res.send('Elisabeth Artistry API')
